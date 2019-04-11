197ef0374e5ae7b19f2cadab2b333c1e83b03db9
Denis Stepanenko <denis.stepanenko@yahoo.com>
2019-04-11 21:07:28 +0100

    Сделал класс "Calculator"

---
+++ programmableCalculator/Calculator.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace programmableCalculator
+{
+    static class Calculator
+    {
+        enum OperationsGroup
+        {
+            MultiplyAndDivide,
+            PlusAndMinus
+        }
+
+        public static double Calculate(string input)
+        {
+            var expression = new Regex(@"(\(|^)[^()]*(\)|$)");
+
+            while (expression.IsMatch(input))
+            {
+                if (Regex.IsMatch(input, @"^[^*/+-]*$"))
+                {
+                    break;
+                }
+
+                var match = expression.Match(input);
+                string subExpression = match.Value;
+
+                if (match.Value[0] == '(' && match.Value[match.Value.Length - 1] == ')')
+                {
+                    subExpression = match.Value.Substring(1, match.Value.Length - 2);
+                }
+
+                input = expression.Replace(input, CalculateSubExpression(subExpression));
+            }
+
+            return Convert.ToDouble(input);
+        }
+
+        static string CalculateSubExpression(string input)
+        {
+            input = CalculateOperations(input, OperationsGroup.MultiplyAndDivide);
+            input = CalculateOperations(input, OperationsGroup.PlusAndMinus);
+
+            return input;
+        }
+
+        static string CalculateOperations(string input, OperationsGroup group)
+        {
+            string pattern = "";
+            if (group == OperationsGroup.MultiplyAndDivide)
+            {
+                pattern = @"((\d+[.,]\d+)|\d+)[*/]((\d+[.,]\d+)|\d+)";
+            }
+
+            if (group == OperationsGroup.PlusAndMinus)
+            {
+                pattern = @"((\d+[.,]\d+)|\d+)[+-]((\d+[.,]\d+)|\d+)";
+            }
+
+            var expression = new Regex(pattern);
+
+            while (expression.IsMatch(input))
+            {
+                var match = expression.Match(input);
+
+                string result = CalculateOperation(match.Value);
+                input = expression.Replace(input, result, 1);
+            }
+
+            return input;
+        }
+
+        static string CalculateOperation(string input)
+        {
+            int separatorIndex;
+            string result = "";
+
+            separatorIndex = input.IndexOf('*');
+
+            if (separatorIndex > -1)
+            {
+                double a = Convert.ToDouble(input.Substring(0, separatorIndex));
+                double b = Convert.ToDouble(input.Substring(separatorIndex + 1, input.Length - separatorIndex - 1));
+                result = (a * b).ToString();
+            }
+
+            separatorIndex = input.IndexOf('/');
+
+            if (separatorIndex > -1)
+            {
+                double a = Convert.ToDouble(input.Substring(0, separatorIndex));
+                double b = Convert.ToDouble(input.Substring(separatorIndex + 1, input.Length - separatorIndex - 1));
+                result = (a / b).ToString();
+            }
+
+            separatorIndex = input.IndexOf('+');
+
+            if (separatorIndex > -1)
+            {
+                double a = Convert.ToDouble(input.Substring(0, separatorIndex));
+                double b = Convert.ToDouble(input.Substring(separatorIndex + 1, input.Length - separatorIndex - 1));
+                result = (a + b).ToString();
+            }
+
+            separatorIndex = input.IndexOf('-');
+
+            if (separatorIndex > -1)
+            {
+                double a = Convert.ToDouble(input.Substring(0, separatorIndex));
+                double b = Convert.ToDouble(input.Substring(separatorIndex + 1, input.Length - separatorIndex - 1));
+                result = (a - b).ToString();
+            }
+
+            return result;
+        }
+    }
+}
--- programmableCalculator/Program.cs
+++ programmableCalculator/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 namespace programmableCalculator
 {
@@ -11,99 +6,23 @@ namespace programmableCalculator
     {
         static void Main(string[] args)
         {
-            /*
-            string input = "(5*(8/(2+2)*(5+5)))";
-
-            var matches = Regex.Matches(input, @"\([^()]*\)");
-
-            foreach(Match match in matches)
+            bool isContinue = false;
+            do
             {
-                Console.WriteLine($"{match.Value.Substring(1, match.Value.Length-2)} {match.Index}");
-            }*/
-            /*
-            string input = "(4*(2+2)*(8/(2+2))/(9+1))";
+                Console.WriteLine(
+@"Доступны операции: *, /, +, - 
+Доступно использование скобок.
+Введите математическое выражение:");
+                string expression = Console.ReadLine();
 
-            string pattern = @"\([^()]*\)";
+                double result = Calculator.Calculate(expression);
+                Console.WriteLine($"Result = {result}");
 
-            while (!Regex.IsMatch(input, @"^((\d*(\.|,)\d*)|\d*)$"))
-            {
-                var evaluator = new MatchEvaluator((x) =>
-                {
-                    Console.WriteLine(x.Value);
-                    return CalculateSubExpression(x.Value.Substring(1, x.Value.Length - 2)).ToString();
-                });
-                input = Regex.Replace(input, pattern, evaluator);
+                Console.Write("Хотите продолжить? (y/n): ");
+                string answer = Console.ReadLine();
+                isContinue = answer == "y" ? true : false;
             }
-
-            */
-            //Console.WriteLine(input);
-
-            Console.WriteLine(CalculateSubExpression("4*4*2/10"));
-            
-            Console.ReadLine();
-        }
-
-
-
-        static double CalculateSubExpression(string input)
-        {
-            string multiplyDividePattern = @"((\d*(\.|,)\d*)|\d+)[*/]((\d*(\.|,)\d*)|\d+)";
-            var multiplyDivideEvaluator = new MatchEvaluator((x) => 
-            {
-                string expression = x.Value;
-                expression = expression.Replace('.', ',');
-                string result = "";
-
-                Console.WriteLine(x.Value);
-
-                if (expression.IndexOf('*') > -1)
-                {
-                    int separatorIndex = expression.IndexOf('*');
-                    double a = Convert.ToDouble(expression.Substring(0, separatorIndex));
-                    double b = Convert.ToDouble(expression.Substring(separatorIndex + 1, expression.Length - separatorIndex - 1));
-                    result = (a * b).ToString();
-                }
-
-                if (expression.IndexOf('/') > -1)
-                {
-                    int separatorIndex = expression.IndexOf('/');
-                    double a = Convert.ToDouble(expression.Substring(0, separatorIndex));
-                    double b = Convert.ToDouble(expression.Substring(separatorIndex + 1, expression.Length - separatorIndex - 1));
-                    result = (a / b).ToString();
-                }
-                
-                return result;
-            });
-            input = Regex.Replace(input, multiplyDividePattern, multiplyDivideEvaluator);
-
-            string plusMinusPattern = @"((\d*(\.|,)\d*)|\d*)[+-]((\d*(\.|,)\d*)|\d*)";
-            var plusMinusEvaluator = new MatchEvaluator((x) =>
-            {
-                string expression = x.Value;
-                expression = expression.Replace('.', ',');
-                string result = "";
-
-                if (expression.IndexOf('+') > -1)
-                {
-                    int separatorIndex = expression.IndexOf('+');
-                    double a = Convert.ToDouble(expression.Substring(0, separatorIndex));
-                    double b = Convert.ToDouble(expression.Substring(separatorIndex + 1, expression.Length - separatorIndex - 1));
-                    result = (a + b).ToString();
-                }
-
-                if (expression.IndexOf('-') > -1)
-                {
-                    int separatorIndex = expression.IndexOf('-');
-                    double a = Convert.ToDouble(expression.Substring(0, separatorIndex));
-                    double b = Convert.ToDouble(expression.Substring(separatorIndex + 1, expression.Length - separatorIndex - 1));
-                    result = (a - b).ToString();
-                }
-
-                return result;
-            });
-            input = Regex.Replace(input, plusMinusPattern, plusMinusEvaluator);
-
-            return Convert.ToDouble(input);
+            while (isContinue);
         }
     }
 }
